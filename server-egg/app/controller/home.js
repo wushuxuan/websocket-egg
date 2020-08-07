e3f21d98f9d5249b7e7da567b1bd98e3f75cc066
wushuxuan <844701878@qq.com>
2020-08-07 17:55:33 +0800

    更新

---
+++ server-egg/app/controller/home.js
@@ -0,0 +1,10 @@
+// app/controller/home.js
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    this.ctx.body = 'Hello world';
+  }
+}
+
+module.exports = HomeController;
